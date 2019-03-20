bc2e354641361956c5674bbb3ad36c38be8a40f5
Максим Бараников <max.baranikov@gmail.com>
2019-03-20 21:56:11 +0300

    Write LoadExcelFActors function

--- ImportExcel/ImportExcel.cs
+++ ImportExcel/ImportExcel.cs
@@ -72,7 +72,46 @@ namespace AccidentForecast
     public class ImportExcel
     {
         //Факторы
-        public static void LoadExcelFactors(string listName, string pathName);
+        public static void LoadExcelFactors(string listName, string pathName)
+        {
+            DataTable dtexcel = new DataTable();
+
+            //string constr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\_LSTU\4 КУРС\1 семестр\Модельки\ЛР1\Лаб1_Тербуны\Абсолютные\Тербуны_влажность.xlsm;Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+            string constr = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;'",pathName);
+            using (OleDbConnection conn = new OleDbConnection(constr))
+            {
+                conn.Open();
+
+                OleDbDataAdapter daexcel = new OleDbDataAdapter(String.Format("Select * from [{0}$]",listName), conn);
+                dtexcel.Locale = CultureInfo.CurrentCulture;
+                daexcel.Fill(dtexcel);
+                conn.Close();
+            }
+            string connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=TestDBAccidents;Data Source=DESKTOP-PPEQF8T";
+            string sql = "insert into Factors values (@A, @B, @C, @D,@E,@F,@G, @H,20)";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                foreach (DataRow row in dtexcel.Rows)
+                {
+                    if (row["Дата"].ToString() == "")
+                        break;
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddWithValue("@A", row["Дата"]);
+                    cmd.Parameters.AddWithValue("@B", row[11]);
+                    cmd.Parameters.AddWithValue("@C", row[12]);
+                    cmd.Parameters.AddWithValue("@D", row[13]);
+                    cmd.Parameters.AddWithValue("@E", row[14]);
+                    cmd.Parameters.AddWithValue("@F", row[15]);
+                    cmd.Parameters.AddWithValue("@G", row[16]);
+                    cmd.Parameters.AddWithValue("@H", row[17]);
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
 
        //ЖКХ
         public static void LoadExcelHaCS(string listName, string pathName);
